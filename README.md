a5827b3a32d927509c3392e3454c4fca7ef4fe10
Rob <rob.mccarthy31@gmail.com>
2022-01-14 16:03:02 +0000

    challenge 1

---
+++ README.md
@@ -0,0 +1,5 @@
+Challenges are located in challenges/
+
+Each challenge folder contains a Makefile which can be used to re-build the challenge binary. Running `make` will execute the re-build and allow you to inspect the various compiler warnings.
+
+gdb is also provided as well as pwntools to help in your exploit development and debugging efforts. 
---
+++ challenges/challenge_1/challenge_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#define BANNER \
+  "Welcome to Challenge 1"
+
+char *gets(char *);
+
+int main(int argc, char **argv) {
+  struct {
+    char buffer[64];
+    volatile int changeme;
+  } locals;
+
+  printf("%s\n", BANNER);
+
+  locals.changeme = 0;
+  gets(locals.buffer);
+
+  if (locals.changeme != 0) {
+    puts("Well done, the 'changeme' variable has been changed!");
+  } else {
+    puts(
+        "Uh oh, 'changeme' has not yet been changed. Would you like to try "
+        "again?");
+  }
+
+  exit(0);
+}
